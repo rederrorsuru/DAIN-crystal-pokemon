800a09476bbd0a37a546bf5b304555da34e9ebf1
rederrorsuru <61506744+rederrorsuru@users.noreply.github.com>
2020-02-26 16:21:36 +0100

    Fix

--- _extract_from_folder.sh
+++ _extract_from_folder.sh
@@ -2,19 +2,18 @@
 #Change the "for file in *limitPallete*.gif;" to change which files you want to extract
 #Change the "cd output_videos" go look in other folders
 #Change the "cp $file ../../"$FILENAME"_dain.gif" line to change the output filename
-echo $@
+#echo Arguments: ["$@"]\n
 for FOLDER in "$@";
 do
     FOLDER2=$(cygpath "$FOLDER" -u -a)
-    #echo Cygpath folder is $FOLDER2
     FILENAME=$(basename -- "$FOLDER2")
-    echo $FOLDER2
+    echo Extracting from $FOLDER2
     cd "$FOLDER2"
     cd output_videos
     for file in *limitPallete*.gif;
     do
-        OUTPUTNAME= "$FILENAME"_dain.gif
-        echo $OUTPUTNAME
+        OUTPUTNAME="$FILENAME"_dain.gif
+        echo Creating $OUTPUTNAME
         cp $file ../../"$OUTPUTNAME"
     done
 done
